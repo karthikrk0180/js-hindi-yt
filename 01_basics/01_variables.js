e2f7ca01d64d2d11ce7a01cad9070f806ecd3e83
Karthik R Khatavkar <126350150+karthikrk0180@users.noreply.github.com>
2025-06-30 17:06:53 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "karthik@gmail.com"
+var accountPassword = "12356"
+accountCity = "Jaipur"
+let accountState;
+
+// Const Behaviour
+// accountId = 2 // not allowed as its constant cant change or alter it.
+console.log(accountId);
+
+// let, var and varibales "values" can be changed/altered later 
+accountEmail = "pragna@gmail.com"
+accountPassword = "09876"
+accountCity = "Hubli"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+// we usually dont use var forget it as var duplicates at all files issues in block scope and functional scope
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)
+// we are using nodejs not browser
+
+console.log(3 + 3);
+
+console.log("Karthik");
+
+let name = "Karthik"
+let age = 12
+let isLoggedIn = true
+
+// number => 2 to power of 53
+// string=> ""
+// Boolean => True/False
+// null => standalone value
+// undefined => 
+// symbol => 
+// object
+
+console.log(typeof undefined) //undefined
+console.log(typeof undefined) // object
