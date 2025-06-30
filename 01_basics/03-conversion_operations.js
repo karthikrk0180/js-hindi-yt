cba80b60a8708023b6fa8b389486a5011137cb75
Karthik R Khatavkar <126350150+karthikrk0180@users.noreply.github.com>
2025-06-30 20:20:44 +0000

    comparison in datatypes

--- 01_basics/03-conversion_operations.js
+++ 01_basics/03-conversion_operations.js
@@ -1,9 +1,9 @@
 let score = "33abc"
-console.log(typeof(score));
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof(valueInNumber))
-console.log(valueInNumber);
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => Nan
@@ -12,14 +12,59 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 let BooleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(BooleanIsLoggedIn);
+// console.log(BooleanIsLoggedIn);
 
 // 1 => true;
 // "" => false
 // "Karthik" => true
 
-let someNumber = 33
+let someNumber = "33"
 let stringNumber = Number(someNumber)
 
-console.log(stringNumber);
-console.log(typeof(stringNumber))
+// console.log(stringNumber);
+// console.log(typeof(stringNumber))
+
+
+let num = 123;
+let str = String(num);
+// console.log(str);              // "123"
+// console.log(typeof str);       // string
+
+
+// ********************************** Operations ***************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "Hello"
+let str2 = " Karthik"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + "2")
+// console.log(1 + 1 + "2")
+// console.log("1" + 1 + 2)
+
+// console.log(+true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
