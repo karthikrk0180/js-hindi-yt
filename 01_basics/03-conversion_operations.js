25a0a2abbb76525927d73d36d1eb8e1b75416a8a
Karthik R Khatavkar <126350150+karthikrk0180@users.noreply.github.com>
2025-06-30 17:29:36 +0000

    convrsion of datatypes

---
+++ 01_basics/03-conversion_operations.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1
+
+let isLoggedIn = 1
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(BooleanIsLoggedIn);
+
+// 1 => true;
+// "" => false
+// "Karthik" => true
+
+let someNumber = 33
+let stringNumber = Number(someNumber)
+
+console.log(stringNumber);
+console.log(typeof(stringNumber))
