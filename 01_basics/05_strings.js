afa17175aaf478c61f20a9c964d04a42a0dd9ecf
Karthik R Khatavkar <126350150+karthikrk0180@users.noreply.github.com>
2025-07-02 07:17:41 +0000

    string datatypes and its proto

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,19 @@
+const name = "Karthik"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello ny name is ${name} and my repo count is ${repoCount}`) //String interpolation 
+
+const gameName = new String('KarthikTopg')
+
+console.log(gameName.__proto__);
+
+console.log(gameName.toUpperCase());
+
+const name1 = "   Karthik  "
+console.log(name1.trim())
+
+const url = "https://karthik/%20khatavkar"
+
+console.log(url.replace('%20','-'))
