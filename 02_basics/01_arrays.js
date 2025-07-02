b4568ee9d683adf2bd1f735214852fbe78a86496
Karthik R Khatavkar <126350150+karthikrk0180@users.noreply.github.com>
2025-07-02 08:32:22 +0000

    arrays and objects

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,41 @@
+// array
+
+const myArr = [0, 1, 2, 3, 5, "karthik", true]
+
+const myHeroes = ["He", "nagarag"]
+
+const myarr2 = new Array(1,2,3)
+
+// console.log(myArr[6]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(9)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+
+myArr.unshift(456)
+// console.log(myArr);
+myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(9))
+// console.log(myArr.indexOf(3))
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+//slice and splice
+
+console.log("A ", myArr)
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+
+
+
+
